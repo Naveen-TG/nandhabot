9c2ee0498e66c7ae1961162b4470e7b1497bf93c
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-10-04 15:56:36 +0530

    Update config.py

--- nandhabot/config.py
+++ nandhabot/config.py
@@ -3,17 +3,17 @@ from pyrogram import Client as bot
 
 
 
-BOT_ID = 5515671520
-BOT_USERNAME = "TrunksRobot"
-BOT_NAME = "TrunksRobot"
-BOT_MENTION = "[TrunksRobot](tg://user?id=5515671520)"
+BOT_ID = 5293794798
+BOT_USERNAME = "Naveen_TG_BoT"
+BOT_NAME = "Naveen-TG BoT"
+BOT_MENTION = "[Naveen_TG_BoT](tg://user?id=5515671520)"
 
-OWNER_ID = 1491497760
+OWNER_ID = 2107036689
 API_ID = 56678
 API_HASH = "nou"
 BOT_TOKEN = "nou"
 UPDATES_CHANNEL = "TrunksUpdates"
 SUPPORT_CHAT = "TrunksSupport"
-LOG_GROUP_ID = "-1001690512977"
+LOG_GROUP_ID = "-1001528043178"
 ARQ_API_KEY = ""
 ARQ_API_URL = "https://arq.hamker.in"
